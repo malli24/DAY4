fb773a05f4092ad2f09f573f5387d40fe31bc49b
malli24 <malleswari82006@gmail.com>
2024-12-05 14:36:12 +0530

    Add files via upload

---
+++ divisior.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class divisior
+{
+    public static void main(String args[])
+    {
+    Scanner Sc=new Scanner(System.in);
+    int t=Sc.nextInt();
+    for(int j=0;j<t;j+=2)
+    {
+        System.out.println(j);
+    }
+   }
+}
