d90ae22d1f3ab782285e7a5f56be2c6bbeb53502
malli24 <malleswari82006@gmail.com>
2024-12-05 14:52:06 +0530

    Add files via upload

---
+++ Foreven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Foreven
+{
+public static void main(String args[])
+{
+    Scanner Sc=new Scanner(System.in);
+    int a=Sc.nextInt();
+    int b=Sc.nextInt();
+for(int i=0;i<=a;i+=2)
+{
+    System.out.println("anu");
+}
+for(int j=0;j<=b;j+=2)
+{
+    System.out.println("stefy");
+}
+}
+}
