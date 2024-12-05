3c878547e7f74e5c97f803f6bee8c2d2f4011ff9
malli24 <malleswari82006@gmail.com>
2024-12-05 12:13:22 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Switch{
+    public static void main(String[]args)
+    {
+    Scanner Sc=new Scanner(System.in);
+    int a=Sc.nextInt();
+    int flag=0;
+    if(a>=18)
+    flag=1;
+    switch(flag)
+    {
+        case 0:
+            System.out.println("not eligible");
+            break;
+        case 1:
+            System.out.println("eligible");
+            break;
+
+    }
+}
+}
