6868fa9990299e892869ca45376e027fedf8fea8
malli24 <malleswari82006@gmail.com>
2024-12-05 14:49:58 +0530

    Add files via upload

---
+++ Count2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Count
+{
+
+    public static void main(String[]args)
+    {
+    Scanner Sc=new Scanner(System.in);
+    int t=Sc.nextInt();
+    int c=0;
+    for(int i=0;i<t;i++)
+    if(n%i==0)
+    {
+        c+=1;
+        if(i*i!=new)
+        {
+            c+=1;
+        }
+    }
+}
+System.out.println(c);
+}
