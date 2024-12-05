cecdaed8289094d58024b1a06e72030d6ab15d54
malli24 <malleswari82006@gmail.com>
2024-12-05 14:43:17 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class For
+{
+public static void main(String args[])
+    {
+       Scanner Sc=new Scanner(System.in);
+       int a=Sc.nextInt();
+       int b=Sc.nextInt();
+       for(int i=0;i<=a;i++)
+    {
+    System.out.println("malli");
+    }
+    for(int j=0;j<=b;j++)
+     {
+    System.out.println("stefy");
+     }
+   }
+}
