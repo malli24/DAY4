31d7cf058a04a3aa8ab401a70f31a45e3813da29
malli24 <malleswari82006@gmail.com>
2024-12-05 15:41:58 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,22 @@
+public class Biggest {
+    public static void main(String args[])
+    {
+        int a=40;
+        int b=50;
+        int c=30;
+        System.out.println("the biggest maong three is");
+        if(a>b && a>c)
+        {
+            System.out.println("a is bigger");
+        }
+    else if(b>c && b>a)
+{
+    System.out.println("b s bigger");
+}
+else if(c>a && c>b)
+{
+    System.out.println("c isbigger");
+}
+}
+    
+}
